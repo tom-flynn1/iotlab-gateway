9e5e58b8c8f4516bfc30664a603fc3a21457a96c
François ROLAND <francois.roland@umons.ac.be>
2018-07-17 11:53:18 +0200

    Bind node-red to any address.

--- nodered/app/settings.js
+++ nodered/app/settings.js
@@ -25,7 +25,7 @@ module.exports = {
     // By default, the Node-RED UI accepts connections on all IPv4 interfaces.
     // The following property can be used to listen on a specific interface. For
     // example, the following would only allow connections from the local machine.
-    //uiHost: "127.0.0.1",
+    uiHost: "0.0.0.0",
 
     // Retry time in milliseconds for MQTT connections
     mqttReconnectTime: 15000,
